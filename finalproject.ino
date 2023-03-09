bc6d7bb3dd0a2427cca38ee06580109b97c0bac7
jencyc1 <91704995+jencyc1@users.noreply.github.com>
2023-03-09 11:16:09 -0800

    Update finalproject.ino

--- finalproject.ino
+++ finalproject.ino
@@ -6,6 +6,9 @@ int remotepin  = 3; // sets up 3 as pin connected to IR remote
 
 // variables:
 int command;
+int sensorValue = 0;   // the sensor value
+int sensorMin = 1023;  // minimum sensor value
+int sensorMax = 0;     // maximum sensor value
 
 IRrecv irrecv(remotepin); // intializes remotepin as IR receiver
 decode_results results; // provides coded results
@@ -21,6 +24,21 @@ void setup() {
   pinMode(gatepin, OUTPUT); // defines ledstrip as output
   pinMode(photoresistor, INPUT); // defines photoresistor as input
   pinMode(remotepin, INPUT); // defines remotepin as input
+
+    // calibrate during the first five seconds - from Arduino example code
+  while (millis() < 5000) {
+    sensorValue = analogRead(photoresistor);
+
+    // record the maximum sensor value
+    if (sensorValue > sensorMax) {
+      sensorMax = sensorValue;
+    }
+
+    // record the minimum sensor value
+    if (sensorValue < sensorMin) {
+      sensorMin = sensorValue;
+    }
+  }
 }
 
 void loop() {
@@ -38,17 +56,38 @@ void loop() {
     }
   } 
 
-  if (command != IR_BUTTON_2) { // if button 2 is not pressed
+  if (command != IR_BUTTON_3) { // if button 3 is not pressed
+    int photo1 = analogRead(photoresistor);
+    int photo2 = analogRead(photoresistor);
+    int photo3 = analogRead(photoresistor);   
+    int photo4 = analogRead(photoresistor);   
+    int photo5 = analogRead(photoresistor);   
+    float avgphoto = ((photo1 + photo2 + photo3 + photo4 + photo5)/5); // takes average of photoresistor values (helps account for fluctuations in light)
     int photo = analogRead(photoresistor); // read and store photoresistor values
+
     Serial.println(photo); // prints photoresistor values so that I can determine when it's light and when it's dark
-    if (photo < 500) { // if the value is less than 500 (the number is calculated based on serial count of light in my hallway.)
-      analogWrite(gatepin, 255); // turns LED strip on.
+
+  sensorValue = analogRead(photoresistor);
+
+  // in case the sensor value is outside the range seen during calibration
+  sensorValue = constrain(sensorValue, sensorMin, sensorMax);
+
+  // apply the calibration to the sensor reading
+  sensorValue = map(sensorValue, sensorMin, sensorMax, 0, 255);
+
+  // fade the LED using the calibrated value:
+    if (avgphoto < sensorMin) {
+      analogWrite(gatepin, 255); // turns LED strip on. // if the value is less than 500 (the number is calculated based on serial count of light in my hallway.)
+      if (avgphoto > sensorMin) { // Naturally, when the LED strip is on, the photoresistor takes in more light. This prevents the LED strip from excessively blinking by identifying max threshold. 
+        analogWrite(gatepin, 255); 
     } 
     else {
       analogWrite(gatepin, 0); // otherwise, turns LED strip off.
     }
   }
 }
+}
+
 
  
 
